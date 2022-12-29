5dee753c3d095afdfe0cbbb2f62bd80c18c19297
Andrea  Balbo Mossetto <andreabalbomossetto@MBP-di-Andrea-2.homenet.telecomitalia.it>
2022-12-29 11:20:26 +0100

    aggiunto sorgente javascript || ancora oggetto di debugging

--- javascript/main.mjs
+++ javascript/main.mjs
@@ -1,39 +1,63 @@
-import Orologio from "./collezionabile.mjs";
+import { Collectible, Watch } from './collectible.mjs';
+import { Participant, Collector} from './participant.mjs';
+
+const collector1 = new Collector('Mario', 'Rossi');
+const collector2 = new Collector('John', 'Doe');
+const participant = new Participant('Akira', 'Toriyama');
+
+/*
+const watch1 = new Watch('Padellone verdone', 'Sporty and anti-magnetic watch', 'Rolex', 'Milgauss', '116400', 2020, 'Steel');
+
+console.log(watch1)
+*/
+
+
+for (let i = 0; i < 10; i++) {
+  const watch = new Watch(`Clock ${i + 1}`, 'A beautiful clock', 'Brand', 'Model', 'Reference', 2020, 'Material');
+  if (i % 2 === 0) {
+    collector1.addToCollection(watch);
+  } else {
+    collector2.addToCollection(watch);
+  }
+}
+
+console.log(collector1.ownCollection);
+console.log(collector2.ownCollection);
+
+participant.takeInLoan(1, 1);
+console.log(participant.available);
+console.log(collector1.ownCollection);
+
+collector2.addToCollection(participant.available[0]);
+console.log(participant.available);
+console.log(collector2.ownCollection);
+
+
+/*
+import Collezionabile from "./collezionabile.mjs";
 import Collezionista from "./collezionista.mjs";
 
-// creo 3 utenti
-const persona1 = new Collezionista("Andrea", "Balbo Mossetto");
-const persona2 = new Collezionista("Eric", "Clapton");
-// const persona3 = new Partecipante("Mario", "Rossi");
+const persona1 = new Collezionista("Ada", "Lovelace");
+const persona2 = new Collezionista("Alan", "Turing");
 
+const elem1 = new Collezionabile("Marchingegno", "Macchina di Turing completa di nastro");
+const elem2 = new Collezionabile("Motore Analitico", "Il primo computer della storia, se solo fosse esistito");
+const elem3 = new Collezionabile("Biro", "Una comune penna a sfera");
+const elem4 = new Collezionabile("Lettera 22", "Una macchina da scrivere, per chi ancora ricorda cosa siano");
+const elem5 = new Collezionabile("33 giri", "Un disco, stanno tornando di moda");
+const elem6 = new Collezionabile("Pellicola fotografica", "Ebbene sì, c'è ancora chi fa fotografie analogiche");
 
-// creo i collezionabili - orologio
-// marchio, modello, referenza, anno
-const orologio1  = new Orologio("Rolex", "Milgauss", "116400", "2020");
-const orologio2  = new Orologio("Omega", "Speedmaster", "493272", "2005");
-const orologio3  = new Orologio("Patek Philippe", "Calatrava", "1145", "1998");
-const orologio4  = new Orologio("Audemars Piguet", "Royal Oak", "15300", "2008");
-const orologio5  = new Orologio("Rolex", "GMT-Master II", "115734", "2015");
-const orologio6  = new Orologio("Omega", "Seamaster", "40382", "1993");
-const orologio7  = new Orologio("Jager Le-Coultre", "Reverso", "230537", "1963");
+persona1.colleziona(elem2);
+persona1.colleziona(elem4);
+persona1.colleziona(elem6);
+persona2.colleziona(elem1);
+persona2.colleziona(elem3);
+persona2.colleziona(elem5);
 
-// assegno gli orologi ai collezionisti
-persona1.colleziona(orologio1);
-persona1.colleziona(orologio2);
-persona1.colleziona(orologio7);
-persona2.colleziona(orologio3);
-persona2.colleziona(orologio4);
-persona2.colleziona(orologio5);
-persona2.colleziona(orologio6);
-
-// printo collezioni di partenza
 console.log("collezioni iniziali");
 console.dir(persona1, {depth: 3});
 console.dir(persona2, {depth: 3});
 
-// operazioni tra gli utenti + stampa stati intermedi
-
-/*
 persona1.presta(elem2, persona2);
 persona2.presta(elem1, persona1);
 persona2.presta(elem3, persona1);
